448ab823e49a95df28f1c9cacbb4da6874c1af2b
Michael Nielsen <michael.1138@gmail.com>
2015-02-25 15:11:49 +0800

    rm hardcoded paths from startup script

--- skel/bin/dev/netbeans-start.sh
+++ skel/bin/dev/netbeans-start.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Use java7/java8+ to run the IDE. Projects can use other platforms.
-export JAVA_HOME=/opt/jdk7
-#export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
+# use Java7/Java8+ to run the IDE; projects can use other platforms
+: ${JAVA_HOME:="${JAVA_HOME:-"/usr/lib/jvm/java-8-openjdk-amd64"}"}
+PATH=$JAVA_HOME/bin:$PATH
 
-# optionally use bundled netbeans maven; just make sure no other maven home is set
+# use bundled netbeans maven; make sure no other maven_home is set
 unset M2_HOME MAVEN_HOME
-export PATH=/opt/maven3/bin:$PATH
 
-# fix java timezone issue. On Linux, if TZ is set, Sun Java always uses it
-export TZ=$(cat /etc/timezone | sed 's/ /_/g')
+# fix Java/Linux timezone issue; if TZ is set, Sun Java always uses it
+[ -f /etc/timezone ] && export TZ=$(cat /etc/timezone | sed 's/ /_/g')
 
 # fix tar using either gnu-tar options, or custom tar wrapper script
 export TAR_OPTIONS=--delay-directory-restore
@@ -20,11 +18,13 @@ export TAR_OPTIONS=--delay-directory-restore
 TMPDIR=$HOME/temp/tmpdir/netbeans
 [ ! -d $TMPDIR ] && mkdir -p $TMPDIR
 
-############################################################################
 # let proxy be set via netbeans options
 unset http_proxy
 unset https_proxy
 unset ftp_proxy
+
+# java classpath, options, properties
+OPTS=
 
 ############################################################################
 # Look-and-Feel examples
@@ -38,61 +38,59 @@ unset ftp_proxy
 #   LAF=" --laf com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
 #   LAF=" --laf com.jgoodies.looks.plastic.PlasticLookAndFeel"
 
-# JGoodies
-LOOKS_HOME=$HOME/opt/lib/jgoodies
-LAF_JARS=$LOOKS_HOME/common/jgoodies-common.jar:$LOOKS_HOME/looks/jgoodies-looks.jar
+# JGoodies (disabled)
+if [ "$JGOODIES" != "" ]; then
+  #LAF=" --laf com.jgoodies.looks.plastic.Plastic3DLookAndFeel"
+  LOOKS_HOME=$HOME/opt/lib/jgoodies
+  LAF_JARS=$LOOKS_HOME/common/jgoodies-common.jar:$LOOKS_HOME/looks/jgoodies-looks.jar
+fi
 
-# disable jgoodies laf
-#CP="-cp:p $LAF_JARS"
-#LAF=" --laf com.jgoodies.looks.plastic.Plastic3DLookAndFeel"
+[ "$LAF_JARS" != "" ] && CP="-cp:p $LAF_JARS" && OPTS="$LAF $CP"
 
-OPTS="$LAF $CP"
-
+############################################################################
 # set classpath and other JVM options, LAF, etc
 #OPTS=" -J-Dswing.aatext=TRUE -J-Dawt.useSystemAAFontSettings=on $OPTS "
 OPTS=" -J-Dorg.netbeans.editor.linewrap=false $OPTS "
 OPTS=" -J-Djsch.connection.timeout=30000 -J-Dsocket.connection.timeout=300000 $OPTS "
 OPTS=" -J-Djava.io.tmpdir=$TMPDIR $OPTS "
 
+############################################################################
 # fix console output truncating junit results
 OPTS=" -J-Dtestrunner.max.msg.line.length=2000 $OPTS "
 
-# for scala
+############################################################################
+# scala
 OPTS=" -J-Ddefault.javac.target=1.6 $OPTS "
 OPTS=" -J-Ddefault.javac.source=1.6 $OPTS "
 # set SCALA_HOME, append "-J-Dscala.home={scala_home_path}" to netbeans options;
 # can set "netbeans_default_options" property in {NETBEANS_HOME}/etc/netbeans.conf
-OPTS=" -J-Dscala.home=$SCALA_HOME $OPTS "
+[ "$SCALA_HOME" != "" ] && OPTS=" -J-Dscala.home=$SCALA_HOME $OPTS "
 
 
 ############################################################################
 # work-around for menus not working; need correct DESKTOP_SESSION
 # for {gnome, mate, etc...} this script guesses at desktop session
-SET_SESSION_SCRIPT=$HOME/bin/desktop_session.sh
-if [ -f $SET_SESSION_SCRIPT ]; then
+: ${SET_SESSION_SCRIPT:="$HOME/bin/desktop_session.sh"}
+if [ -f "$SET_SESSION_SCRIPT" ]; then
+  echo "## script to fix desktop session: $SET_SESSION_SCRIPT" 1>&2
   eval $( $SET_SESSION_SCRIPT -v -p -u gnome )
-else
-  echo "*** warning: script to fix desktop session not found: $SET_SESSION_SCRIPT" 1>&2
-  echo "*** warning: setting desktop session:  DESKTOP_SESSION=gnome (was: $DESKTOP_SESSION)" 1>&2
-  export DESKTOP_SESSION=gnome  # using script instead by default
 fi
 
 # work-around for older gnome desktop (ubuntu) and remote dev host
 # http://wiki.netbeans.org/FaqCplusPlusRemoteSocketException
-# (my desktop-session script above now does this)
+# (the desktop_session.sh script (above) now also fixes this)
 #unset GNOME_DESKTOP_SESSION_ID
 #unset MATE_DESKTOP_SESSION_ID
 
-NETBEANS_HOME=/opt/netbeans-dev
+############################################################################
+: ${NETBEANS_HOME:="/opt/netbeans"}
 
 echo "## $(readlink -e $NETBEANS_HOME/bin/netbeans)"
 echo "## OPTS=$OPTS"
 echo "## CP=$CP"
 echo "## DESKTOP_SESSION=$DESKTOP_SESSION"
 
-#echo ## starting gradle..."
-#type gradle && gradle --daemon --quiet
-#sleep 3
+#type gradle && echo ## starting gradle..." && gradle --daemon --quiet
 
 echo ## starting netbeans..."
 set -x
